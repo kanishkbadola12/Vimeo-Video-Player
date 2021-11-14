1a16420a5220b3ec4fd155ed3c22bc74efc17598
Badola <kanishk.badola@sap.com>
2021-11-14 23:15:46 +0530

    Intial setup of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'VideoPlayer';
+  @ViewChild('videoTemplate') videoTemplate!: ElementRef;
+  public title: string = 'VideoPlayer - BHF';
+  constructor(private http: HttpClient) { }
+
+  /**
+  * fetch video from Vimeo API using video URL
+  * @param {String} videoURL
+  */
+  public getVideo(videoURL: string) {
+    let oEmbedURL = "https://vimeo.com/api/oembed.json?url=" + videoURL;
+    this.http.get(oEmbedURL).subscribe(
+      (videoData: any) => {
+        console.log(videoData);
+        this.videoTemplate.nativeElement.innerHTML = videoData.html;
+      }
+    )
+  }
 }
+
+
