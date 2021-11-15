ceff0c92e4f900a9783047dc8933da4458029e27
Badola <kanishk.badola@sap.com>
2021-11-15 19:54:49 +0530

    functionality and style development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NgxSpinnerService } from "ngx-spinner";  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,20 +9,33 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   @ViewChild('videoTemplate') videoTemplate!: ElementRef;
   public title: string = 'VideoPlayer - BHF';
-  constructor(private http: HttpClient) { }
+  public showError: Boolean = false;
+  public videoInfo: String = '';
+  constructor(
+    private http: HttpClient,
+    private spinner: NgxSpinnerService
+    ) { }
 
   /**
   * fetch video from Vimeo API using video URL
   * @param {String} videoURL
   */
   public getVideo(videoURL: string) {
-    let oEmbedURL = "https://vimeo.com/api/oembed.json?url=" + videoURL;
-    this.http.get(oEmbedURL).subscribe(
-      (videoData: any) => {
-        console.log(videoData);
-        this.videoTemplate.nativeElement.innerHTML = videoData.html;
-      }
-    )
+    let oEmbedURL = "https://vimeo.com/api/oembed.json?url=" + videoURL + "&width=600&height=300";
+    this.showError = false;
+    if (videoURL !== '') {
+      this.spinner.show(); 
+      this.http.get(oEmbedURL).subscribe(
+        (videoData: any) => {
+          console.log(videoData);
+          this.spinner.hide(); 
+          this.videoTemplate.nativeElement.innerHTML = videoData.html;
+          this.videoInfo = videoData.title + ' by ' + videoData.author_name;
+        }
+      )
+    } else {
+      this.showError = true;
+    }
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { NgxSpinnerModule } from "ngx-spinner"; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
