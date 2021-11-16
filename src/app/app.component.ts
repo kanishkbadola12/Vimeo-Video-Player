20c5a1d65d587d32e2ec4492e808729b5bbef36f
Badola <kanishk.badola@sap.com>
2021-11-16 16:47:49 +0530

    Error handling and comments addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,20 @@ import { NgxSpinnerService } from "ngx-spinner";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  /* View child decorator to inject template reference for iFrame of video */
   @ViewChild('videoTemplate') videoTemplate!: ElementRef;
+
+  /* Variable declaration with defined types */
   public title: string = 'VideoPlayer - BHF';
-  public showError: Boolean = false;
+  public emptyFieldError: Boolean = false;
+  public httpError: Boolean = false;
   public videoInfo: String = '';
+  public httpErroInfo: String = '';
+
   constructor(
-    private http: HttpClient,
-    private spinner: NgxSpinnerService
+    private http: HttpClient,   //dependency injection for HttpClientModule
+    private spinner: NgxSpinnerService    //dependency injection for NgxSpinner
     ) { }
 
   /**
@@ -22,19 +29,26 @@ export class AppComponent {
   */
   public getVideo(videoURL: string) {
     let oEmbedURL = "https://vimeo.com/api/oembed.json?url=" + videoURL + "&width=600&height=300";
-    this.showError = false;
     if (videoURL !== '') {
-      this.spinner.show(); 
+      //disable errors
+      this.emptyFieldError = false;
+      this.httpError = false;
+      this.spinner.show();      //show spinner untill data is not fetched
       this.http.get(oEmbedURL).subscribe(
         (videoData: any) => {
-          console.log(videoData);
-          this.spinner.hide(); 
+          this.spinner.hide();    //hide spinner when data is fetched
           this.videoTemplate.nativeElement.innerHTML = videoData.html;
           this.videoInfo = videoData.title + ' by ' + videoData.author_name;
+        },
+        //error handling
+        (errorResponse: any) => {
+          this.spinner.hide();
+          this.httpError = true;
+          this.httpErroInfo = errorResponse.error;
         }
       )
     } else {
-      this.showError = true;
+      this.emptyFieldError = true;
     }
   }
 }
